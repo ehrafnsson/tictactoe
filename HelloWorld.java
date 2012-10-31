d85ff9eec06a134140bfcfd1133ea5d93b38f9ce
Arni Birgisson <arnib@arnib.net>
2012-10-31 17:30:34 +0000

    la la la

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld extends JFrame
 	public HelloWorld()
 	{
 	JPanel panel1 = new JPanel();
-	JLabel label1 = new JLabel("Hello, World, this is my first Java program on Ubuntu Linux");
+	JLabel label1 = new JLabel("Hello, World, this is my first Java program on Mac!");
 	panel1.add(label1);
 	this.add(panel1);
 	this.setTitle("Hello World");
