20472f61b9c1f15b021a801e7fab0a4c72518651
Sigurður Snær Eiríksson <sse1987@gmail.com>
2012-11-21 13:14:37 +0000

    Útfærði föll í GameEngine og fiktaði aðeins í GUI
    Ég sá að það var eiginlega búinn að redda GUI þannig að ég reyndi bara
    að breyta eða stækka fontinn í GUI en það vildi ekki virka þannig að ég
    leyfi ykkur að fikta í því.
    
    Hinsvegar útfærði ég GameEngine þokkalega og núna er hægt að spila
    leikinn og allt svo sem virkar :-)
    
    Kv. Siggi

--- src/com/fjarverandi/tictactoe/GameEngine.java
+++ src/com/fjarverandi/tictactoe/GameEngine.java
@@ -1,25 +1,91 @@
 package com.fjarverandi.tictactoe;
 
-import java.awt.*;
+import java.awt.Point;
 
 public class GameEngine implements iEngine {
+	/*
+	 * true = player one turn
+	 * false = player two turn
+	 */
+	private boolean playerOneTurn = true;
+	/*
+	 * 0 = empty
+	 * 1 = player one (X)
+	 * 2 = player two (O)
+	 */
+	private byte[][] board = { {0, 0, 0}, 
+							   {0, 0, 0}, 
+							   {0, 0, 0}
+							 };
+	
     public boolean Set (Point point)
     {
-        return true;
+    	if (board[point.x][point.y] == 0)
+    	{
+    		board[point.x][point.y] = (byte) (playerOneTurn ? 1 : 2);
+    		playerOneTurn = (playerOneTurn ? false : true);
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
     }
     public byte Get (Point point)
     {
-        return 1;
+        return board[point.x][point.y];
     }
     public byte CheckVictory()
     {
-        return 0;
+    	// Check if player one has won
+    	if 		(board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1) return 1;
+    	else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) return 1;
+    	else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1) return 1;
+    	else if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1) return 1;
+    	else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) return 1;
+    	else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1) return 1;
+    	else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) return 1;
+    	else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1) return 1;
+    	
+    	// Check if player two has won
+    	if 		(board[0][0] == 2 && board[1][0] == 2 && board[2][0] == 2) return 2;
+    	else if (board[0][1] == 2 && board[1][1] == 2 && board[2][1] == 2) return 2;
+    	else if (board[0][2] == 2 && board[1][2] == 2 && board[2][2] == 2) return 2;
+    	else if (board[0][0] == 2 && board[0][1] == 2 && board[0][2] == 2) return 2;
+    	else if (board[1][0] == 2 && board[1][1] == 2 && board[1][2] == 2) return 2;
+    	else if (board[2][0] == 2 && board[2][1] == 2 && board[2][2] == 2) return 2;
+    	else if (board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) return 2;
+    	else if (board[2][0] == 2 && board[1][1] == 2 && board[0][2] == 2) return 2;
+    	
+    	// If someone is empty then the game is still ON!
+    	for (int y = 0; y < 3; y++)
+    	{
+    		for (int x = 0; x < 3; x++)
+    		{
+    			if (board[y][x] == 0) return 0;
+    		}
+    	}
+    	
+    	// If all spot is full and nether of the players has won then it's a draw!
+    	return 3;
     }
 
-    public void NewGame() {}
+    public void NewGame()
+    {
+    	// Set playerOneTurn to default
+    	playerOneTurn = true;
+    	// Set board to default
+    	for (int y = 0; y < 3; y++)
+    	{
+    		for (int x = 0; x < 3; x++)
+    		{
+    			board[y][x] = 0;
+    		}
+    	}
+    }
 
     public byte PlayerTurn()
     {
-        return 0;
+        return (byte) (playerOneTurn ? 1 : 2);
     }
 }
--- src/com/fjarverandi/tictactoe/TicTacToePanel.java
+++ src/com/fjarverandi/tictactoe/TicTacToePanel.java
@@ -1,13 +1,18 @@
 package com.fjarverandi.tictactoe;
 
+import javax.jws.soap.SOAPBinding.Style;
 import javax.swing.*;
+import javax.swing.text.StyleConstants;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class TicTacToePanel extends JPanel {
-
-    final String[] PlayerText = { "", "X", "O" };
+public class TicTacToePanel extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	
+	final String[] PlayerText = { "", "X", "O" };
     private JLabel label;
     private Point point;
     private iEngine ie;
@@ -16,6 +21,7 @@ public class TicTacToePanel extends JPanel {
     {
         this.setSize(200, 200);
         this.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.black));
+        this.setFont(new Font("Times New Roman", Font.BOLD, 72));
         this.point = new Point(i%3,i/3);
         this.ie = ie;
 
