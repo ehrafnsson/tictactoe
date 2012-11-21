c0d23de8fb09fc3485fcfcf99cb8d0a6b162911c
Arni Birgisson <arnib@arnib.net>
2012-11-21 17:53:57 +0000

    Menubar and "File" menu added + removing un-used imports and cleanup

--- src/com/fjarverandi/tictactoe/GUI.java
+++ src/com/fjarverandi/tictactoe/GUI.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame
 {
-    private iEngine ie;
+	private static final long serialVersionUID = 1L;
+
+	private iEngine ie;
     private TicTacToePanel[] panels;
 
     public GUI(iEngine ie)
