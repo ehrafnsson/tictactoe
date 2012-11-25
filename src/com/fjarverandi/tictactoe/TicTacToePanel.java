a39bf57b530502f599a6bb54bec6e1596365556f
Arni Birgisson <arnib@arnib.net>
2012-11-25 19:55:41 +0000

    Enlarged font and centralized

--- src/com/fjarverandi/tictactoe/TicTacToePanel.java
+++ src/com/fjarverandi/tictactoe/TicTacToePanel.java
@@ -1,9 +1,6 @@
 package com.fjarverandi.tictactoe;
 
-import javax.jws.soap.SOAPBinding.Style;
 import javax.swing.*;
-import javax.swing.text.StyleConstants;
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -21,45 +18,44 @@ public class TicTacToePanel extends JPanel
     {
         this.setSize(200, 200);
         this.setBorder(BorderFactory.createBevelBorder(0, Color.black, Color.black));
-        this.setFont(new Font("Times New Roman", Font.BOLD, 72));
         this.point = new Point(i%3,i/3);
         this.ie = ie;
 
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.CENTER;
+        gridbag.setConstraints(this, constraints);
+        this.setLayout(gridbag);
+
         label = new JLabel();
+        label.setFont(new Font("Times New Roman", Font.BOLD, 90));
         this.add(label);
 
         this.addMouseListener(new MouseListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                JPanel jp = (JPanel) e.getSource();
-
+            public void mouseClicked(MouseEvent e)
+            {
                 if(ie.Set(point))
                 {
                     refresh();
                     parent.StateChanged();
                 }
-
-
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
         });
 
